cc128228ae37222ba21ddbd740006b14ef3bfa95
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2017-07-13 16:39:26 +0800

    result = requests.get(url, headers=headers) html = result.text # content会是raw字节，这时对它decode可以是中文

--- crw/testEncode/test.py
+++ crw/testEncode/test.py
@@ -1,3 +1,4 @@
+# windows 上面你 python test.py '你好' 它会把两个引号当成raw内容传进去 linux不传
 import sys 
 
 str = sys.argv[1] 
@@ -10,4 +11,3 @@ print('你好'.encode('utf-8'))
 print(str.encode('utf-8')) 
 
 #https://www.v2ex.com/t/352585#reply13
-# windows 上面你 python test.py '你好' 它会把两个引号当成raw内容传进去 linux不传
